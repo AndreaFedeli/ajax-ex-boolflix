62a06e1475c39ee7bd90aee77075023e09f26eac
AndreaFedeli <andrea.fedeli159@gmail.com>
2020-09-02 16:02:24 +0200

    milestone 1 def

--- script.js
+++ script.js
@@ -1,55 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 
 
-$('button').click(function(){
-  var input= $('input:text').val();
-  console.log(input);
+  $('button').click(function() {
+    var input = $('input:text').val();
+    console.log(input);
 
-  $.ajax(
-    { url: 'https://api.themoviedb.org/3/search/movie',
-      method : 'GET',
-      data : {
+    $.ajax({
+      url: 'https://api.themoviedb.org/3/search/movie',
+      method: 'GET',
+      data: {
         api_key: 'c830462730db14d8b93291f626fbdf9e',
         query: 'input',
         language: 'it-IT',
       },
-      success : function(data) {
-      printMovie(data.results);
+      success: function(data) {
+        printMovie(data.results);
 
-    },
-    'error': function() {
-     alert('Errore ');
-    }
-});
-
-
-//funzione per l inserimento dei dati ricavati da API
-
-    function printMovie(movie){
-      for (var i = 0; i <movie.length; i++) {
-      var thisMovie= movie.results;
-      console.log(movie[i]);
+      },
+      'error': function() {
+        alert('Errore ');
       }
+    });
 
 
-    var source = $("#entry-template").html();
-    var template = Handlebars.compile(source);
+    //funzione per l inserimento dei dati ricavati da API
 
-    var context = {
-            title: thisMovie[i].title,
-            original_title: thisMovie[i].original_title,
-            original_language: thisMovie[i].original_language,
-            vote_average: thisMovie[i].vote_average
+    function printMovie(movie) {
+      var source = $("#entry-template").html();
+      var template = Handlebars.compile(source);
+      for (var i = 0; i < movie.length; i++) {
+        var thisMovie = movie;
+        console.log(thisMovie);
+
+
+        var context = {
+          title: thisMovie.title,
+          original_title: thisMovie.original_title,
+          original_language: thisMovie.original_language,
+          vote_average: thisMovie.vote_average
         };
         var html = template(context);
         $('.movie-container').append(html);
       }
 
-
-
-
-});
-
-
+    };
+  });
 });
